1245e1e773f2096e51abab6554bb0f7aafbbc60b
Gareth <gsveitch@gmail.com>
2017-12-30 17:56:52 -0600

    [bootstrap] install bootsrap and jquery. render login button to page

--- src/js/app.js
+++ src/js/app.js
@@ -6,9 +6,23 @@ import '../css/style.css';
 import micImage from '../assets/Mic_NoMeter.png';
 
 export default class Hello extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    // this.onLoginClick = () => {
+    //   console.log('LETS LOGIN !')
+    // };
+  }
+  onLoginClick() {
+    console.log('LETS LOGIN !')
+  }
+
   render() {
     return (
       <div>
+        <div align="right">
+          <button type="button" className="btn btn-lg btn-primary" onClick={this.onLoginClick}>Login</button>
+        </div>
         <h1>shush</h1>
         <br/>
         <canvas id="meter" width="300" height="50"></canvas>
