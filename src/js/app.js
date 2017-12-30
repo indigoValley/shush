96468ab97cb22b9c4f34a28eb5ff1b3e323b3ab2
Gareth <gsveitch@gmail.com>
2017-12-30 11:49:42 -0600

    [webpack] setup and configure webpack to compile and bundle js, jsx, css, and image files. install react and make splash page in src folder

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import '../css/style.css';
+
+import micImage from '../assets/microphone.png';
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>shush</h1>
+        <br/>
+        <img src={micImage} alt='microphone' className="displayed" width="400px"/>
+      </div>
+    );
+  }
+}
+
+render(<Hello />, document.getElementById('app'));
