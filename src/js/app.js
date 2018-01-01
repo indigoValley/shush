10181489a358af4b6073e524eaa6baed8592b1f7
Gareth <gsveitch@gmail.com>
2017-12-31 18:38:09 -0600

    [meter] refactor volume-meter.js and export loadVolume function to App.js to access mic level stream

--- src/js/app.js
+++ src/js/app.js
@@ -7,25 +7,40 @@ import '../meter/main';
 
 import micImage from '../assets/Mic_NoMeter.png';
 import LoginControl from './LoginControl.jsx';
+import {loadVolume} from '../meter/volume-meter';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isLoggedIn: null,
+      vol: 0
+
+    };
   }
   onLoginClick() {
     console.log('LETS LOGIN !')
   }
 
+  //use throttle here?
+  componentWillMount() {
+    loadVolume((vol) => {
+      // this.setState({currentVol: vol});
+      console.log(vol);
+
+    });
+  }
+
   render() {
+    const {isLoggedIn, name} = this.state;
     return (
       <div>
         <h1>shush</h1>
+        {/* CONDITIONAL RENDERING SYNTAX --> {isLoggedIn && <LoggedInUser name={name} />} */}
         <div align="right">
-          <LoginControl />
+          <button type="button" className="btn btn-lg btn-primary" onClick={this.onLoginClick}>Login</button>
         </div>
         <br/>
-        {/* <VolumeMeter component goes here? */}
         <canvas id="meter" width="300" height="50"></canvas>
         <br/>
         <img src={micImage} alt='microphone' className="displayed" width="300px"/>
